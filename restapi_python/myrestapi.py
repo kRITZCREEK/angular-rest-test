f0cf924bb3ae0011b70cea90e63932c499ca1641
kRITZCREEK <christoph.hegemann@gmx.net>
2014-01-07 18:42:42 +0100

    List of Values

--- restapi_python/myrestapi.py
+++ restapi_python/myrestapi.py
@@ -23,9 +23,10 @@ def put_document():
         abort(400, 'No _id specified')
     try:
         db['documents'].save(entity)
+        addToList(entity['_id'])
     except ValidationError as ve:
         abort(400, str(ve))
-     
+
 @route('/documents/:id', method='GET')
 def get_document(id):
     entity = db['documents'].find_one({'_id':id})
@@ -40,7 +41,48 @@ def remove_document(id):
         abort(404, 'No document with id %s' % id)
     try:
         db['documents'].remove({'_id':id})
+        removeFromList(entity['_id'])
     except ValidationError as ve:
         abort(400, str(ve))
+        
+
+@route('/list', method='GET')
+def get_list():
+    entity = db['list'].find_one({'_id':'list'})
+    if not entity:
+        abort(404, 'Internal Server Error: (List unavailable)')
+    return entity
+
+@route('/list', method='PUT')
+def init_list():
+    try:
+        db['list'].save({'_id': 'list', 'list' : []})
+    except ValidationError as ve:
+        abort(400, str(ve))
+    
+def addToList(newID):
+    print newID
+    _list = get_list()['list']
+    print 'Alte Liste: ' + str(_list)
+    _list.append(newID)
+    print 'Neue Liste: ' + str(_list)
+    try:
+        #db['list'].remove({'_id':'list'})
+        db['list'].save({'_id': 'list', 'list' : _list})
+    except ValidationError as ve:
+        abort(400, str(ve))
+
+def removeFromList(oldID):
+    print oldID
+    _list = get_list()['list']
+    print 'Alte Liste: ' + str(_list)
+    _list.remove(oldID)
+    print 'Neue Liste: ' + str(_list)
+    try:
+        #db['list'].remove({'_id':'list'})
+        db['list'].save({'_id': 'list', 'list' : _list})
+    except ValidationError as ve:
+        abort(400, str(ve))
+    
         
 run(host='localhost', port=8080)
