ce36b70831b0a7150e19bd85815b6f0fa3962774
kRITZCREEK <christoph.hegemann@gmx.net>
2014-01-07 09:20:34 +0100

    Added Delete Operation

--- restapi_python/myrestapi.py
+++ restapi_python/myrestapi.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  6 19:07:55 2014
+
+@author: christoph
+"""
+
 import json
 import bottle
 from bottle import route, run, request, abort
@@ -26,4 +33,14 @@ def get_document(id):
         abort(404, 'No document with id %s' % id)
     return entity
  
+@route('/documents/:id', method='DELETE')
+def remove_document(id):
+    entity = db['documents'].find_one({'_id':id})
+    if not entity:
+        abort(404, 'No document with id %s' % id)
+    try:
+        db['documents'].remove({'_id':id})
+    except ValidationError as ve:
+        abort(400, str(ve))
+        
 run(host='localhost', port=8080)
